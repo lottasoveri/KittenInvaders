9263a02aebea4b144ef0869a50977c78cad6329b
Lotta Soveri <64742851+lottasoveri@users.noreply.github.com>
2024-06-04 02:28:01 +0300

    Add player and an invader

--- kitteninvaders.py
+++ kitteninvaders.py
@@ -6,6 +6,7 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 960))
 pygame.display.set_caption("Kitten Invaders")
 clock = pygame.time.Clock()
+font = pygame.font.Font(None, 30)
 
 # Starfield generator:
 star_coords = []
@@ -19,18 +20,26 @@ while star_counter < 200:
     star_counter += 1
     
 # Game surfaces:
-starfield_surf = pygame.Surface((screen.get_width(), screen.get_height()-50))
-starfield_surf.fill((0,0,25))
+starfield = pygame.Surface((screen.get_width(), screen.get_height()-50))
+starfield.fill((0,0,25))
 for star in star_coords:
-    pygame.draw.circle(starfield_surf, (204, 229, 255), (star["star_x"], star["star_y"]), star["star_size"])
+    pygame.draw.circle(starfield, (204, 229, 255), (star["star_x"], star["star_y"]), star["star_size"])
     
-ground_surf = pygame.Surface((screen.get_width(), 50))
-ground_surf.fill((64, 64, 64))
+ground = pygame.Surface((screen.get_width(), 50))
+ground.fill((64, 64, 64))
+
+score_counter = font.render("counter", False, (255, 255, 255))
 
 # Game characters:  
-player = pygame.image.load("images/cannon.png")
-kitty1 = pygame.image.load("images/kitty1.png")
-bolt = pygame.image.load("images/piu.png")
+player_surf = pygame.image.load("images/cannon.png").convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (screen.get_width()/2, screen.get_height()-50))
+
+kitty1_surf = pygame.image.load("images/kitty1.png").convert_alpha()
+kitty_x = randrange(10, screen.get_width()-10)
+kitty1_rect = kitty1_surf.get_rect(midbottom = (kitty_x, -10))
+
+bolt_surf = pygame.image.load("images/piu.png").convert_alpha()
+bolt_rect = bolt_surf.get_rect()
 
 # Game control initiation:
 right = False
@@ -59,17 +68,23 @@ while True:
             if event.key == pygame.K_RIGHT:
                 right = False
                 
-    # if right:
-    #     if player.x < 1280 - player.get_width():
-    #         player.x += 3
-    # if left:
-    #     if player.x > 0:
-    #         player.x -= 3
-    
-    screen.blit(starfield_surf, (0, 0))
-    screen.blit(ground_surf, (0, screen.get_height()-50))            
-    screen.blit(player, (screen.get_width()/2 - player.get_width()/2, screen.get_height() - player.get_height() - 50))
-    
-    pygame.display.flip()
-    
+    if right:
+        if player_rect.right <= screen.get_width():
+            player_rect.x += 5
+    if left:
+        if player_rect.left > 0:
+            player_rect.x -= 5
+            
+    kitty1_rect.y += 1
+    if kitty1_rect.top >= screen.get_height():
+        kitty_x = randrange(10, screen.get_width()-10)
+        kitty1_rect.midbottom = (kitty_x, -10)
+            
+    screen.blit(starfield, (0, 0))
+    screen.blit(kitty1_surf, kitty1_rect)
+    screen.blit(ground, (0, screen.get_height()-50))            
+    screen.blit(score_counter, (10, screen.get_height()-35))
+    screen.blit(player_surf, player_rect)
+
+    pygame.display.update()
     clock.tick(60)
