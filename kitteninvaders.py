c1d1ce26271c43e044162d5888cc907f49ebb08e
Lotta Soveri <64742851+lottasoveri@users.noreply.github.com>
2024-06-05 01:04:01 +0300

    add bolt-enemy collisions and some images

--- kitteninvaders.py
+++ kitteninvaders.py
@@ -5,17 +5,45 @@ def enemy_movement(enemy_list: list):
     if enemy_list:
         for enemy_rect in enemy_list:
             enemy_rect.y += 1
-            screen.blit(kitty1_surf, enemy_rect)
+            if enemy_rect.height < 100:
+                screen.blit(kitty2_surf, enemy_rect)
+            else:
+                screen.blit(kitty1_surf, enemy_rect)    
+        enemy_list = [enemy for enemy in enemy_list if enemy.y < (screen.get_height() + 100)]    
         return enemy_list
     else:
         return []
+    
+def firing_bolts(bolt_list: list):
+    if bolt_list:
+        for bolt_rect in bolt_list:
+            bolt_rect.y -= 5
+            screen.blit(bolt_surf, bolt_rect)
+        bolt_list = [bolt for bolt in bolt_list if bolt.y > -20]    
+        return bolt_list
+    else:
+        return []
+        
+def bolt_collision(bolts, enemies):
+    if enemies:
+        for enemy_rect in enemies:
+            for bolt_rect in bolts:
+                if enemy_rect.colliderect(bolt_rect):
+                    enemy_rect.y = screen.get_height() +100
+                    bolt_rect.y = -20
 
+def keeping_score():
+    score_surf = font.render(f"Score: {game_score}", False, (0, 0, 25))
+    score_rect = score_surf.get_rect(center = (screen.get_width()/2, screen.get_height()-25))
+    screen.blit(score_surf, score_rect)
+    
 pygame.init()
 
 screen = pygame.display.set_mode((1280, 960))
 pygame.display.set_caption("Kitten Invaders")
 clock = pygame.time.Clock()
 font = pygame.font.Font(None, 30)
+game_score = 0
 
 # Starfield generator:
 star_coords = []
@@ -37,20 +65,18 @@ for star in star_coords:
 ground = pygame.Surface((screen.get_width(), 50))
 ground.fill((64, 64, 64))
 
-score_surf = font.render(" 0 ", False, (0, 0, 25))
-score_rect = score_surf.get_rect(center = (screen.get_width()/2, screen.get_height()-25))
-
 # Player:  
 player_surf = pygame.image.load("images/cannon.png").convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (screen.get_width()/2, screen.get_height()-50))
 
+# Cannon bolts:
 bolt_surf = pygame.image.load("images/piu.png").convert_alpha()
-bolt_rect = bolt_surf.get_rect(center = (screen.get_width(), screen.get_height()))
+
+bolt_rect_list = []
 
 # Enemies:
 kitty1_surf = pygame.image.load("images/kitty1.png").convert_alpha()
-kitty_x = randrange(20, screen.get_width()-20)
-kitty1_rect = kitty1_surf.get_rect(midbottom = (kitty_x, -10))
+kitty2_surf = pygame.image.load("images/kitty2.png").convert_alpha()
 
 enemy_rect_list = []
 
@@ -77,7 +103,7 @@ while True:
             if event.key == pygame.K_RIGHT:
                 right = True
             if event.key == pygame.K_SPACE:
-                pass
+                bolt_rect_list.append(bolt_surf.get_rect(midbottom = player_rect.midtop))
             
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
@@ -86,7 +112,7 @@ while True:
                 right = False
                 
         if event.type == enemy_timer:
-            enemy_rect_list.append(kitty1_surf.get_rect(midbottom = (randrange(20, screen.get_width()-20), -10)))
+            enemy_rect_list.append(choice([kitty1_surf, kitty2_surf]).get_rect(midbottom = (randrange(20, screen.get_width()-20), -10)))
                 
     if right:
         if player_rect.right <= screen.get_width():
@@ -94,23 +120,25 @@ while True:
     if left:
         if player_rect.left > 0:
             player_rect.x -= 5
-            
-    # kitty1_rect.y += 1
-    # if kitty1_rect.top >= screen.get_height():
-    #     kitty_x = randrange(20, screen.get_width()-20)
-    #     kitty1_rect.midbottom = (kitty_x, -10)
-            
+    
+    # Space background:        
     screen.blit(starfield, (0, 0))
-    screen.blit(bolt_surf, bolt_rect)
-    # screen.blit(kitty1_surf, kitty1_rect)
     
     # Enemy movement:
     enemy_rect_list = enemy_movement(enemy_rect_list)
     
-    screen.blit(ground, (0, screen.get_height()-50))    
-    pygame.draw.rect(screen, (150, 150, 150), score_rect) 
-    screen.blit(score_surf, score_rect)
+    # Bolt movement
+    bolt_rect_list = firing_bolts(bolt_rect_list)
+    
+    # Collisions
+    bolt_collision(bolt_rect_list, enemy_rect_list)
+        
     screen.blit(player_surf, player_rect)
-
+    
+    screen.blit(ground, (0, screen.get_height()-50))    
+ 
+    # Game score:
+    keeping_score()
+    
     pygame.display.update()
     clock.tick(60)
