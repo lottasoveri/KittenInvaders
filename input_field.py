b78a3a21be61480f6e45c2ed0b20865754196b29
Lotta Soveri <64742851+lottasoveri@users.noreply.github.com>
2024-08-06 23:37:53 +0300

    add increasing enemy projectiles upon game progression, slight code cleanup

--- input_field.py
+++ input_field.py
@@ -2,8 +2,7 @@ import pygame
 
 pygame.font.init()
 
-color_inactive = pygame.Color((128, 128, 128))
-color_active = pygame.Color((0, 255, 255))
+border_color = pygame.Color((0, 255, 255))
 font = pygame.font.Font(None, 30)
 
 class InputField:
@@ -11,31 +10,22 @@ class InputField:
         self.pos_x = screen_width/2 - width/2
         self.pos_y = screen_height/2
         self.rect = pygame.Rect(self.pos_x, self.pos_y, width, height)
-        self.color = color_inactive
+        self.color = border_color
         self.text = text
         self.text_surface = font.render(text, True, (204, 204, 204))
-        self.active = False
 
     # User input:    
     def handle_events(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            # If player clicks in input field:
-            if self.rect.collidepoint(event.pos):
-                self.active = True
-            else:
-                self.active = False
-            self.color = color_active if self.active else color_inactive
         if event.type == pygame.KEYDOWN:
-            if self.active:
-                if event.key == pygame.K_BACKSPACE:
-                    if len(self.text) > 0:
-                        self.text = self.text[:-1]
-                elif len(self.text) < 20:
-                        self.text += event.unicode
-                self.text_surface = font.render(self.text, True, (204, 204, 204))
+            if event.key == pygame.K_BACKSPACE:
+                if len(self.text) > 0:
+                    self.text = self.text[:-1]
+            elif len(self.text) < 20:
+                    self.text += event.unicode
+            self.text_surface = font.render(self.text, True, (204, 204, 204))
                 
     def clear(self):
-        self.text = self.text.strip()
+        self.text = ""
         self.text_surface = font.render(self.text, True, (204, 204, 204))
 
     def draw(self, screen):
